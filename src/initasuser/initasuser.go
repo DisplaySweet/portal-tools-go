71346aac417e625bb02837b4b4a568b07a7e0cb2
Patrick <patrick.mitchell95@gmail.com>
2018-05-04 11:06:29 +1000

    Attempt to incorporate user auth

---
+++ src/initasuser/initasuser.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+
+	"github.com/DisplaySweet/portal-go/src"
+)
+
+func main() {
+	temp, err := ioutil.ReadFile("./.user-creds.json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	ua := &portal.UserAuth{}
+	json.Unmarshal(temp, ua)
+
+	temp, err = ioutil.ReadFile("./.nv-session.json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	s := &portal.Session{}
+	json.Unmarshal(temp, s)
+
+	err = s.AuthAsUser(ua)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
---
+++ src/prospectbuyers/prospectbuyers.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+
+	"github.com/DisplaySweet/portal-go/src"
+)
+
+func main() {
+
+	temp, err := ioutil.ReadFile("./.nv-session.json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	s := &portal.Session{}
+	json.Unmarshal(temp, s)
+
+	companyNames, err := returnProspectBuyers(s)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	for _, company := range companyNames {
+		fmt.Println(company)
+	}
+}
+
+func returnProspectBuyers(s *portal.Session) ([]string, error) {
+
+	pb, err := s.GetProspectBuyers()
+	if err != nil {
+		return nil, err
+	}
+
+	list := make([]string, 0, 0)
+
+	for _, buyer := range pb {
+		str := fmt.Sprintf("%v : %v %v", buyer.ID, buyer.Firstname, buyer.Lastname)
+		list = append(list, str)
+	}
+
+	return list, nil
+}
